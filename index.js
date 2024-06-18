086c521e4027ea8fe8ba8844787c10bac112a217
John Pascual <ashleypascual@hotmail.es>
2024-06-18 10:28:57 +0100

    task 3

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ function multiplyAndAdd3(num1, num2) {
   return (num1 * num2) + 3;
 }
 
-// Task 3
+// Task 3: Calculate how many times 1000 needs to be halved to be less than 10
+function halvingToLimit(num, limit) {
+  let count = 0;
+  while (num >= limit) {
+    num = halveNumber(num);
+    count++;
+  }
+  return count;
+}
+
 
 // Task 4
 
